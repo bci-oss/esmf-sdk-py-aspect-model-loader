3c6e9672168dd7d1d8de1b1e679d5ebee6f4fa51
Hanna Shalamitskaya (EPAM) <external.Hanna.Shalamitskaya@de.bosch.com>
2025-04-11 17:57:56 +0500

    Added tests

---
+++ core/esmf-aspect-meta-model-python/tests/integration/test_aspect_functionality.py
@@ -0,0 +1,148 @@
+#  Copyright (c) 2023 Robert Bosch Manufacturing Solutions GmbH
+#
+#  See the AUTHORS file(s) distributed with this work for additional
+#  information regarding authorship.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+#   SPDX-License-Identifier: MPL-2.0
+
+from os import getcwd
+from pathlib import Path
+
+from esmf_aspect_meta_model_python import BaseImpl, SAMMGraph
+
+RESOURCE_PATH = getcwd() / Path("tests/integration/resources/org.eclipse.esmf.test.general/2.2.0")
+
+
+def test_get_access_path():
+    file_path = RESOURCE_PATH / "Movement.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+    path = samm_graph.determine_element_access_path(aspect.properties[2].data_type.properties[2])
+
+    assert path[0][0] == "position"
+    assert path[0][1] == "z"
+
+    path = samm_graph.determine_access_path("y")
+
+    assert path[0][0] == "position"
+    assert path[0][1] == "y"
+
+    path = samm_graph.determine_access_path("x")
+
+    assert path[0][0] == "position"
+    assert path[0][1] == "x"
+
+
+def test_get_access_path_input_property():
+    file_path = RESOURCE_PATH / "AspectWithOperationNoOutput.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+    path = samm_graph.determine_element_access_path(aspect.operations[0].input_properties[0])
+
+    assert path[0][0] == "input"
+
+    path = samm_graph.determine_element_access_path(aspect.operations[1].input_properties[0])
+
+    assert path[0][0] == "input"
+
+
+def test_find_properties_by_name() -> None:
+    file_path = RESOURCE_PATH / "AspectWithProperties.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    _ = samm_graph.load_aspect_model()
+
+    result = samm_graph.find_by_name("testPropertyOne")
+    assert result is not None
+    assert len(result) == 1
+    assert isinstance(result[0], BaseImpl)
+    assert result[0].name == "testPropertyOne"
+    assert result[0].urn == "urn:samm:org.eclipse.esmf.test.general:2.2.0#testPropertyOne"
+    assert len(result[0].preferred_names) == 0
+    assert len(result[0].see) == 0
+    assert len(result[0].descriptions) == 0
+
+    result = samm_graph.find_by_name("testPropertyTwo")
+    assert result is not None
+    assert len(result) == 1
+    assert isinstance(result[0], BaseImpl)
+    assert result[0].name == "testPropertyTwo"
+    assert result[0].urn == "urn:samm:org.eclipse.esmf.test.general:2.2.0#testPropertyTwo"
+    assert len(result[0].preferred_names) == 0
+    assert len(result[0].see) == 0
+    assert len(result[0].descriptions) == 0
+
+    result = samm_graph.find_by_name("Unknown")
+    assert len(result) == 0
+
+
+def test_find_property_chaticaristic_by_name() -> None:
+    file_path = RESOURCE_PATH / "AspectWithPropertyWithAllBaseAttributes.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+    _ = aspect.properties[0].characteristic
+    result = samm_graph.find_by_name("BooleanTestCharacteristic")
+
+    assert result is not None
+    assert len(result) == 1
+    assert isinstance(result[0], BaseImpl)
+    assert result[0].name == "BooleanTestCharacteristic"
+    assert result[0].urn == "urn:samm:org.eclipse.esmf.test.general:2.2.0#BooleanTestCharacteristic"
+    assert len(result[0].preferred_names) == 0
+    assert len(result[0].see) == 0
+    assert len(result[0].descriptions) == 0
+
+
+def test_find_properties_by_urn() -> None:
+    file_path = RESOURCE_PATH / "AspectWithProperties.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    _ = samm_graph.load_aspect_model()
+    element = samm_graph.find_by_urn("urn:samm:org.eclipse.esmf.test.general:2.2.0#testPropertyOne")
+
+    assert element is not None
+    assert isinstance(element, BaseImpl)
+    assert element.name == "testPropertyOne"
+    assert element.urn == "urn:samm:org.eclipse.esmf.test.general:2.2.0#testPropertyOne"
+    assert len(element.preferred_names) == 0
+    assert len(element.see) == 0
+    assert len(element.descriptions) == 0
+
+    element = samm_graph.find_by_urn("urn:samm:org.eclipse.esmf.test.general:2.2.0#testPropertyTwo")
+    assert element is not None
+    assert isinstance(element, BaseImpl)
+    assert element.name == "testPropertyTwo"
+    assert element.urn == "urn:samm:org.eclipse.esmf.test.general:2.2.0#testPropertyTwo"
+    assert len(element.preferred_names) == 0
+    assert len(element.see) == 0
+    assert len(element.descriptions) == 0
+
+    element = samm_graph.find_by_urn("Unknown")
+    assert element is None
+
+
+def test_find_property_characteristic_by_urn() -> None:
+    file_path = RESOURCE_PATH / "AspectWithPropertyWithAllBaseAttributes.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+    _ = aspect.properties[0].characteristic
+    element = samm_graph.find_by_urn("urn:samm:org.eclipse.esmf.test.general:2.2.0#BooleanTestCharacteristic")
+
+    assert element is not None
+    assert isinstance(element, BaseImpl)
+    assert element.name == "BooleanTestCharacteristic"
+    assert element.urn == "urn:samm:org.eclipse.esmf.test.general:2.2.0#BooleanTestCharacteristic"
+    assert len(element.preferred_names) == 0
+    assert len(element.see) == 0
+    assert len(element.descriptions) == 0
+
+    element = samm_graph.find_by_urn("Unknown")
+    assert element is None
---
+++ core/esmf-aspect-meta-model-python/tests/integration/test_characteristics.py
@@ -0,0 +1,251 @@
+#  Copyright (c) 2023 Robert Bosch Manufacturing Solutions GmbH
+#
+#  See the AUTHORS file(s) distributed with this work for additional
+#  information regarding authorship.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+#   SPDX-License-Identifier: MPL-2.0
+
+from os import getcwd
+from pathlib import Path
+
+import rdflib
+
+from esmf_aspect_meta_model_python import (
+    Collection,
+    Duration,
+    Enumeration,
+    Measurement,
+    Quantifiable,
+    SAMMGraph,
+    State,
+    StructuredValue,
+)
+
+RESOURCE_PATH = getcwd() / Path("tests/integration/resources/org.eclipse.esmf.test.characteristics/2.2.0")
+
+
+def test_loading_aspect_with_collection():
+    file_path = RESOURCE_PATH / "AspectWithCollection.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    assert aspect.name == "AspectWithCollection"
+    assert aspect.get_preferred_name("en") == "Test Aspect"
+    assert aspect.get_description("en") == "This is a test description"
+    assert aspect.operations == []
+    assert aspect.see == ["http://example.com/"]
+
+    aspect_property = aspect.properties[0]
+    assert aspect_property.name == "testProperty"
+    assert aspect_property.get_preferred_name("en") == "Test Property"
+    assert aspect_property.get_description("en") == "This is a test property."
+    assert sorted(aspect_property.see) == ["http://example.com/", "http://example.com/me"]
+    assert aspect_property.urn == "urn:samm:org.eclipse.esmf.test.characteristics:2.2.0#testProperty"
+    assert aspect_property.example_value == rdflib.Literal("Example Value")
+
+    characteristic = aspect_property.characteristic
+    assert characteristic.name == "TestCollection"
+    assert characteristic.get_preferred_name("en") == "Test Collection"
+    assert characteristic.get_description("en") == "This is a test collection."
+    assert characteristic.see == ["http://example.com/"]
+
+    data_type = characteristic.data_type
+    assert data_type.is_scalar is True
+    assert data_type.is_complex is False
+    assert data_type.urn == "http://www.w3.org/2001/XMLSchema#string"
+
+
+def test_loading_aspect_with_set():
+    file_path = RESOURCE_PATH / "AspectWithSet.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    assert aspect is not None
+
+
+def test_loading_aspect_with_sorted_set():
+    file_path = RESOURCE_PATH / "AspectWithSortedSet.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    assert aspect is not None
+
+
+def test_loading_aspect_with_list():
+    file_path = RESOURCE_PATH / "AspectWithList.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    assert aspect is not None
+
+
+def test_loading_aspect_with_collection_with_element_characteristic():
+    file_path = RESOURCE_PATH / "AspectWithCollectionWithElementCharacteristic.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    collection_characteristic = first_property.characteristic
+    assert isinstance(collection_characteristic, Collection)
+
+    data_type = collection_characteristic.data_type
+    assert data_type.urn == "http://www.w3.org/2001/XMLSchema#string"
+
+    element_characteristic = collection_characteristic.element_characteristic
+    assert element_characteristic.urn == "urn:samm:org.eclipse.esmf.samm:characteristic:2.2.0#Text"
+
+    assert element_characteristic.parent_elements[0].urn == (
+        "urn:samm:org.eclipse.esmf.test.characteristics:2.2.0#TestCollection"
+    )
+
+
+def test_loading_aspect_with_simple_enum():
+    file_path = RESOURCE_PATH / "AspectWithSimpleEnum.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    enum_characteristic = first_property.characteristic
+    assert isinstance(enum_characteristic, Enumeration)
+    assert enum_characteristic.name == "testPropertyOne_characteristic"
+
+    values = enum_characteristic.values
+    assert len(values) == 3
+    assert "foo" in values
+    assert "bar" in values
+    assert "baz" in values
+
+
+def test_loading_aspect_with_simple_state():
+    print(RESOURCE_PATH)
+    file_path = RESOURCE_PATH / "AspectWithState.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    state_characteristic = first_property.characteristic
+    assert isinstance(state_characteristic, State)
+    assert state_characteristic.name == "testPropertyOne_characteristic"
+
+    values = state_characteristic.values
+    assert len(values) == 3
+    assert "default" in values
+    assert "good" in values
+    assert "bad" in values
+    default = state_characteristic.default_value
+    assert default == "default"
+
+
+def test_loading_aspect_with_quantifiable():
+    file_path = RESOURCE_PATH / "AspectWithQuantifiableAndUnit.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    quantifiable_characteristic = first_property.characteristic
+    assert quantifiable_characteristic.name == "TestQuantifiable"
+
+    assert isinstance(quantifiable_characteristic, Quantifiable)
+    unit = quantifiable_characteristic.unit
+    assert unit is not None
+    assert unit.urn == "urn:samm:org.eclipse.esmf.samm:unit:2.2.0#hertz"
+    assert unit.symbol == "Hz"
+    assert unit.code == "HTZ"
+    assert unit.reference_unit is None
+    assert unit.conversion_factor is None
+    assert len(unit.quantity_kinds) == 1
+    for quantity_kind in unit.quantity_kinds:
+        assert quantity_kind.name == "frequency"
+    assert unit.parent_elements[0].urn == "urn:samm:org.eclipse.esmf.test.characteristics:2.2.0#TestQuantifiable"
+
+
+def test_loading_aspect_with_duration():
+    file_path = RESOURCE_PATH / "AspectWithDuration.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    duration_characteristic = first_property.characteristic
+    assert isinstance(duration_characteristic, Duration)
+    assert duration_characteristic.name == "TestDuration"
+
+    assert duration_characteristic.unit.urn == "urn:samm:org.eclipse.esmf.samm:unit:2.2.0#kilosecond"
+
+
+def test_loading_aspect_with_measurement():
+    file_path = RESOURCE_PATH / "AspectWithMeasurement.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    measurement_characteristic = first_property.characteristic
+    assert isinstance(measurement_characteristic, Measurement)
+    assert measurement_characteristic.name == "TestMeasurement"
+
+    assert measurement_characteristic.unit.urn == "urn:samm:org.eclipse.esmf.samm:unit:2.2.0#kelvin"
+
+
+def test_loading_aspect_with_structured_value():
+    file_path = RESOURCE_PATH / "AspectWithStructuredValue.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    structured_value_characteristic = first_property.characteristic
+    assert isinstance(structured_value_characteristic, StructuredValue)
+    assert structured_value_characteristic.name == "StructuredDate"
+
+    assert structured_value_characteristic.deconstruction_rule == "(\\d{4})-(\\d{2})-(\\d{2})"
+    elements = structured_value_characteristic.elements
+    assert len(elements) == 5
+
+    year = elements[0]
+    assert year.name == "year"
+    dash_one = elements[1]
+    assert dash_one == "-"
+    month = elements[2]
+    assert month.name == "month"
+    dash_two = elements[3]
+    assert dash_two == "-"
+    day = elements[4]
+    assert day.name == "day"
+
+
+def test_loading_aspect_with_code():
+    file_path = RESOURCE_PATH / "AspectWithCode.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    code_characteristic = first_property.characteristic
+    assert code_characteristic.name == "TestCode"
+    assert code_characteristic.data_type.urn == "http://www.w3.org/2001/XMLSchema#int"
+
+
+def test_loading_aspect_with_blank_node() -> None:
+    file_path = RESOURCE_PATH / "AspectWithBlankNode.ttl"
+    samm_graph = SAMMGraph()
+    samm_graph.parse(file_path)
+    aspect = samm_graph.load_aspect_model()
+
+    first_property = aspect.properties[0]
+    assert first_property.name == "list"
+    either_characteristic = first_property.characteristic
+    assert either_characteristic is not None
+    assert either_characteristic.name == "list_characteristic"
