618a0d3ad2341c673f4fe09ef3705d4a93a5dfd3
Hanna Shalamitskaya (EPAM) <external.Hanna.Shalamitskaya@de.bosch.com>
2025-11-03 13:36:28 +0500

    Update SAMM CLI version number

--- core/esmf-aspect-meta-model-python/scripts/download_samm_cli.py
+++ core/esmf-aspect-meta-model-python/scripts/download_samm_cli.py
@@ -1,8 +1,8 @@
 """Download SAMM CLI.
 
-Windows: https://github.com/eclipse-esmf/esmf-sdk/releases/download/v2.10.2/samm-cli-2.10.2-windows-x86_64.zip
-  Linux: https://github.com/eclipse-esmf/esmf-sdk/releases/download/v2.10.2/samm-cli-2.10.2-linux-x86_64.tar.gz
-    JAR: https://github.com/eclipse-esmf/esmf-sdk/releases/download/v2.10.2/samm-cli-2.10.2.jar
+Windows: https://github.com/eclipse-esmf/esmf-sdk/releases/download/v2.11.1/samm-cli-2.11.1-windows-x86_64.zip
+  Linux: https://github.com/eclipse-esmf/esmf-sdk/releases/download/v2.11.1/samm-cli-2.11.1-linux-x86_64.tar.gz
+    JAR: https://github.com/eclipse-esmf/esmf-sdk/releases/download/v2.11.1/samm-cli-2.11.1.jar
 """
 
 from esmf_aspect_meta_model_python.samm_cli.download import download_samm_cli
